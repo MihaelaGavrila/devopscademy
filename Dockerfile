f6fb02b67e74b44d0399f6c6a0490f47a00044f6
Mihaela <gavrilamc31@gmail.com>
2022-11-08 17:10:54 +0200

    Homework from module 2 and 3 - Mihaela Gavrila

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+From ubuntu:latest
+COPY first_script_hw1.sh / 
+CMD ["/bin/bash", "/first_script_hw1.sh"]
---
+++ first_script_hw1.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+#This is my first homework
+
+#Show current user
+u=$USER
+echo "The current user is: $u"
+
+#Show current working directory
+echo -e "\n"
+echo "The current working directory is: $(pwd)"
+
+#Show all the files inside the current directory
+echo -e "\n"
+echo "The files in the current working directory: $(ls)"
+
+#Show the current date
+echo -e "\n"
+echo "Current date: $(date)"
+ 
